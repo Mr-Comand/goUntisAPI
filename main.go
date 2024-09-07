d55052ac20af3cd30b136845b557876ce48cdb03
Mr_Comand <74143930+Mr-Comand@users.noreply.github.com>
2024-09-07 13:47:18 +0200

    added main.go example

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/Mr-Comand/goUntisAPI/structs"
+	"github.com/Mr-Comand/goUntisAPI/untisApi"
+)
+
+func main() {
+	apiConfig := structs.ApiConfig{Server: "", User: "", Password: "", Useragent: "client", School: "FannyLGym"}
+	c := untisApi.NewClient(apiConfig)
+	err := c.Authenticate()
+	if err != nil {
+		fmt.Println("Error authenticating:", err)
+	}
+	c.Test()
+	data, err := c.GetClassesOfSchoolYear(1)
+	dataJson, _ := json.Marshal(data)
+	if err != nil {
+		fmt.Println("Error marshaling person slice:", err)
+		return
+	}
+	fmt.Println(string(dataJson), err)
+	c.Logout()
+}
